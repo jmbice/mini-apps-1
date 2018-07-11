4aac0e5fa663aa0b26266c1b2ddc66acb6c98b54
Jordan Bice <jmbice@gmail.com>
2018-07-10 20:52:49 -0700

    Order by unique identifier, list parent_ids

--- challenge_2/server/work.js
+++ challenge_2/server/work.js
@@ -21,39 +21,59 @@ var getLatest = function(callback) {
 
 var parseToCSV = function(json) {
   var all = [];
-  var uniqueId = 0;
+  var uniqueId = 1;
 
   var getTableHeader = function(obj) {
     var first = [];
-    first.push('Unique Id');
+    first.push('Unique_ID');
     var allKeys = Object.keys(obj);
     for (var i = 0; i < allKeys.length; i++) {
       if (allKeys[i] !== 'children') {
         first.push(allKeys[i]);
       }
     }
-    return first;
+    first.push('Parent_ID');
+    return first.join(',');
   }
 
-  all.push(getTableHeader(json).join(','));
-
-  var recursivelyParse = function (obj) {
+  var recursivelyParse = function (obj, parentID) {
     var innerArray = [];
+    innerArray.push(uniqueId)
+    uniqueId++;
     for (key in obj) {
       if (key !== 'children') {
         innerArray.push(obj[key]);
       } else if (key === 'children' && obj[key].length > 0) {
         for (var j = 0; j < obj[key].length; j++){
-          recursivelyParse(obj[key][j]);
+          recursivelyParse(obj[key][j], innerArray[0]);
         }
       }
     }
-    all.push(innerArray.join(','));
+    innerArray.push(parentID);
+    all.push(innerArray);
   }
 
-  recursivelyParse(json);
+  var sortById = function(totalArray) {
+    var tableHeader = totalArray[0];
+    var tableRows = totalArray.slice(1);
+    tableRows.sort((a, b) => {
+      return a[0] - b[0];
+    });
 
-  return all.join(' <br />');
+    console.log(tableRows);
+    var sortedArray = [];
+    sortedArray.push(tableHeader);
+    tableRows.forEach((e) => {
+      sortedArray.push(e.join(','));
+    });
+
+    return sortedArray;
+  };
+
+  all.push(getTableHeader(json));
+  recursivelyParse(json);
+  var sorty = sortById(all);
+  return sorty.join('<br/>');
 }
 
 module.exports = {
